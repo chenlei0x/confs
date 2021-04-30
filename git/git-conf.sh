cc6541c868db3bd17ab6f3c1bf11cde625eb973c
lennychen <lennychen@tencent.com>
2021-04-30 15:37:16 +0800

    git: add push simplt

--- git/git-conf.sh
+++ git/git-conf.sh
@@ -2,3 +2,4 @@ git config --global user.name  "lennychen"
 git config --global user.email "lennychen@tencent.com"
 git config --global credential.helper store
 git config --global pager.branch false
+git config --global push.default current
