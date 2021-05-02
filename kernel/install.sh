bcec158b4a2d68dcd09dd53d109c7eb74146e9ea
lennychen <lennychen@tencent.com>
2021-05-02 22:56:17 +0800

    kernel: add dwarves package

--- kernel/install.sh
+++ kernel/install.sh
@@ -2,5 +2,5 @@
 
 mkdir -p /data/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS} || true
 touch /data/.rpmmacros || true
-yum install asciidoc audit-libs-devel binutils-devel elfutils-devel newt-devel numactl-devel openssl-devel python2-devel xmlto
+yum install asciidoc audit-libs-devel binutils-devel elfutils-devel newt-devel numactl-devel openssl-devel python2-devel xmlto dwarves
 
