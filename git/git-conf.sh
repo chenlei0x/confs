a78f1c00509e69121976d7c559cbcc10d3b95c88
lennychen <lennychen@tencent.com>
2021-05-11 10:21:44 +0800

    git: push

--- git/git-conf.sh
+++ git/git-conf.sh
@@ -3,3 +3,4 @@ git config --global user.email "lennychen@tencent.com"
 git config --global credential.helper store
 git config --global pager.branch false
 git config --global push.default current
+git config --global pull.ff only
