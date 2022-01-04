5815a230895be7b42ef5e63ce3c8b9081bb59e6c
Lei Chen <lennychen@tencent.com>
2022-01-04 16:33:49 +0800

    1

--- tmux-backup/00-tmux.install.sh
+++ tmux-backup/00-tmux.install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-
+install_path="/opt/tmux"
 wget https://github.com/antontkv/tmux-appimage/releases/download/3.2a/tmux-3.2a-x86_64.appimage
-mkdir -p /opt/tmux/bin
-mv tmux-3.2a-x86_64.appimage /opt/tmux/bin/tmux
-chmod a+x /opt/tmux/bin/tmux
+mkdir -p /opt/tmux/
+mv tmux-3.2a-x86_64.appimage $install_path/tmux
+chmod a+x $install_path/tmux
